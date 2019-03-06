f7a317d0f4bc03e18ea4fda685d9e5e3f46971c0
Milos Mirkovic <35512001+milosmirkovic484484@users.noreply.github.com>
2019-03-06 01:19:50 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  $(".menu_btn img").click(function(){
+      $(".menu").toggle();
+  });
+});
+
+
+
+
+$(window).load(function() {
+    $('.flexslider').flexslider({
+      animation: "slide"
+    });
+  });
